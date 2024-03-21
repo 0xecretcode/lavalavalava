5c42850d4cf8eb8252ae79ae71af38d992d745cd
0xecret Code <118183737+0xecretcode@users.noreply.github.com>
2024-03-22 00:43:42 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-b7a025d5-d68f-445d-9196-1345b2cc2b29/)
 
 
 echo "$curl_output" >> Update.tsx
